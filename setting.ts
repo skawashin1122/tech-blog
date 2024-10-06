4e9e21202cfcb45cd1a7733c1f95f7e5f4eec4ed
skawashin1122 <941574_teacher@g.miyazaki-c.ed.jp>
2024-10-06 20:41:15 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://skawashin1122.github.io/tech-blog/";
